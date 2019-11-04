a546ed2752e4f0eb1cb35df4764ae2d324e65346
LAPTOP-CSKAJUBA\junichi <jm615jm615@gmail.com>
2019-11-04 23:31:28 +0900

    おためし

--- src/reply.js
+++ src/reply.js
@@ -4,12 +4,31 @@ exports.handler = async function(event, context, callback) {
   const webhookBody = JSON.parse(event.body)
   console.log(webhookBody)
 
+
+var data = ['アンジェ', 'プリンセス', 'ドロシー', 'ベアトリス', 'ちせ'];
+ 
+var getData = choose_at_random(data);
+console.log(getData);
+ 
+/**
+ * 配列の値からランダムで1つ選択して返す
+ * @param arr arrayData (required) 選択する配列の内容
+ * @return str
+ */
+function choose_at_random(arrayData) {
+    var arrayIndex = Math.floor(Math.random() * arrayData.length);
+    return arrayData[arrayIndex];
+}
+
+
+
   const data = {
     replyToken: webhookBody.events[0].replyToken,
     messages: [
       {
         type: 'text',
-        text: 'ネガティブに決まってるだろ'
+        //text: 'ネガティブに決まってるだろ'
+        text: getData
       }
     ]
   }
